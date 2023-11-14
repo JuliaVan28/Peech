1e878b8afd1597844a5036c752cbf5b47f96c111
Yuliia Vanchytska <julia.van2801@gmail.com>
2023-11-14 14:00:46 +0100

    Initial Commit

---
+++ Peech/PeechApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeechApp.swift
+//  Peech
+//
+//  Created by Yuliia on 14/11/23.
+//
+
+import SwiftUI
+
+@main
+struct PeechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
